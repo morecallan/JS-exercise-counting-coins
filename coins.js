9b5993d14248d756d11bb0bafd8070974e67543c
Callan Morrison <callanmorrison@Callans-MacBook-Pro.local>
2016-01-30 18:32:00 -0600

    updated js file to divide out coins

--- coins.js
+++ coins.js
@@ -1,25 +1,33 @@
-/*
-  Input: 0.67
-  Output:
-  {
-    quarters: 2,
-    dimes: 1,
-    nickels: 1,
-    pennies: 2
-  }
-
-*/
+var coinPurseDOM = document.getElementById("coinPurse");
 
 
-
-function coinCounter () {
+function coinCounter (money) {
   // Initialize a JavaScript object to hold the coins
   var coinPurse = {};
+  
+  money = money*100
 
-  coinPurse.quarters = 0;
+  var remainderQuarters = money % 25;
+  coinPurse.quarters = (money - remainderQuarters) / 25;
+  var minusQuarters = money - (coinPurse.quarters *25);
+  var remainderDimes = minusQuarters % 10;
+  coinPurse.dimes = (minusQuarters - remainderDimes) / 10;
+  var minusDimes = minusQuarters - (coinPurse.dimes * 10);
+  var remainderNickels = minusDimes % 5;
+  coinPurse.nickels = (minusDimes - remainderNickels) / 5;
+  var minusNickels = minusDimes - (coinPurse.nickels * 5);
+  coinPurse.pennies = minusNickels / 1;
+
+  coinPurseDOM.innerHTML += "<li>" + "Quarters: " + coinPurse.quarters + "</li>";
+  coinPurseDOM.innerHTML += "<li>" + "Dimes: " + coinPurse.dimes + "</li>";
+  coinPurseDOM.innerHTML += "<li>" + "Nickels: " + coinPurse.nickels + "</li>";
+  coinPurseDOM.innerHTML += "<li>" + "Pennies: " + coinPurse.pennies + "</li>";
 
   return coinPurse;
 }
 
-var coins = coinCounter()
-console.log();
+var userInput = prompt("List an amount you want divided into coins. (eg: 1.67) ");
+var userInputParsed = parseFloat(userInput);
+coinCounter(userInputParsed);
+
+
